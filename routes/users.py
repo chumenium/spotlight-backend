630c14cb0d51a3ec18b9a371021e2691e972e412
kei-25 <h.keita0829@gmail.com>
2025-10-30 13:57:31 +0900

    不具合解消3

--- routes/users.py
+++ routes/users.py
@@ -207,26 +207,27 @@ def change_icon():
         print(file)
         if file:
             # ===== Base64文字列のヘッダーを除去 =====
-            match = re.match(r"^data:image\/(png|jpeg|jpg|webp|gif);base64,(.+)$", file)
-            if not match:
-                return jsonify({
-                    "status": "error",
-                    "message": "不正な画像データです。"
-                }), 400
+            # match = re.match(r"^data:image\/(png|jpeg|jpg|webp|gif);base64,(.+)$", file)
+            # if not match:
+            #     return jsonify({
+            #         "status": "error",
+            #         "message": "不正な画像データです。"
+            #     }), 400
 
-            ext = match.group(1)
-            img_data = base64.b64decode(match.group(2))
+            # ext = match.group(1)
+            # img_data = base64.b64decode(match.group(2))
 
             # ===== 保存パス設定 =====
             save_dir = os.path.join(current_app.root_path, "icon")
             os.makedirs(save_dir, exist_ok=True)
 
-            filename = f"{username}_icon.{ext}"
+            filename = f"{username}_icon.png"
             save_path = os.path.join(save_dir, filename)
 
             # ===== 画像を保存 =====
+# Base64 → バイナリ書き込み
             with open(save_path, "wb") as f:
-                f.write(img_data)
+                f.write(base64.b64decode(file))
         else:
             filename = "default_icon.jpg"
         iconimgpath = f"/icon/{filename}"
