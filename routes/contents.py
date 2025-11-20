8c1aaf5cc0db05d4caf6ba14bde10a2ecb6ad0be
kei-25 <98150453+kei-25@users.noreply.github.com>
2025-11-20 16:03:15 +0900

    コンソール表示ログ追加(通知)

--- routes/contents.py
+++ routes/contents.py
@@ -173,6 +173,7 @@ def add_comment():
             posted_by_user_data = get_user_by_parentcomment_id(contentID, parentcommentid)
             if posted_by_user_data["notificationenabled"]:
                 send_push_notification(posted_by_user_data["token"], "コメントが投稿されました","あなたが投稿したコメントに"+post_username+"さんがコメントを投稿しました")
+                print(f"{posted_by_user_data["username"]}に通知を送信")
             insert_notification(userID=posted_by_user_data["userID"],comCTID=contentID,comCMID=commentid)
         else:
             commentid = insert_comment(
@@ -185,6 +186,7 @@ def add_comment():
             if content_user_data["notificationenabled"]:
                 title = content_user_data["title"]
                 send_push_notification(content_user_data["token"], "コメントが投稿されました",title+"に"+post_username+"さんがコメントを投稿しました")
+                print(f"{content_user_data["username"]}に通知を送信")
             insert_notification(userID=content_user_data,comCTID=contentID,comCMID=commentid)
 
         return jsonify({"status": "success", "message": "コメントを追加しました。"}), 200
@@ -270,6 +272,7 @@ def spotlight_on_route():
             spotlight_user = get_user_by_id(uid)
             title = content_user_data["title"]
             send_push_notification(content_user_data["token"], "スポットライトが当てられました",title+"に"+spotlight_user["username"]+"さんがスポットライトを当てました")
+            print(f"{content_user_data["username"]}に通知を送信")
         insert_notification(userID=content_user_data["userID"],contentuserCID=contentID,contentuserUID=spotlight_user["userID"])
         return jsonify({"status": "success", "message": "スポットライトをONにしました"}), 200
     except Exception as e:
