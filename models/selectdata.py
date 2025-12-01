7208898f8adae872dffb5d6006b8296d2619aa64
kei-25 <h.keita0829@gmail.com>
2025-12-01 14:13:20 +0900

    親コメント主情報取得エラー修正

--- models/selectdata.py
+++ models/selectdata.py
@@ -64,7 +64,7 @@ def get_user_by_parentcomment_id(contentID,parentcommentID):
         conn = get_connection()
         with conn.cursor() as cur:
             cur.execute(
-                'SELECT u.userID, u.username, u.iconimgpath, u.token, u.notificationenabled FROM coment c JOIN "user" u ON c.userID = u.userID WHERE c.contentID = %s contentID = %s',
+                'SELECT u.userID, u.username, u.iconimgpath, u.token, u.notificationenabled FROM comment c JOIN "user" u ON c.userID = u.userID WHERE c.contentID = %s AND c.commentID = %s',
                 (contentID,parentcommentID)
             )
             row = cur.fetchone()
