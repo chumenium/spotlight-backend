99017d7b42b82f4278318490e49e57798a2270fc
kei-25 <h.keita0829@gmail.com>
2025-12-03 11:12:02 +0900

    DBの変更とタグの処理変更

--- routes/contents.py
+++ routes/contents.py
@@ -116,7 +116,7 @@ def add_content():
         title = data.get("title")
         link = data.get("link")
         tag = data.get("tag")
-
+        tag = tag.replace("#", "")
         if content_type != "text":
 
             file_data = data.get("file")         # base64文字列（コンテンツ本体）
