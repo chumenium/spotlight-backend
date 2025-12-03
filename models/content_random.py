d29cff8049d358b35b9b8470da766164aa4c2f26
kei-25 <h.keita0829@gmail.com>
2025-12-03 15:01:10 +0900

    ランダム取得SQL作成1

--- models/content_random.py
+++ models/content_random.py
@@ -25,12 +25,25 @@ def get_recent_history_ids(uid):
                         SELECT lastcontetid
                         FROM "user"
                         WHERE userID = %s
+                        UNION
+                        SELECT c.contentID
+                        FROM content c
+                        JOIN "user" u ON c.userID = u.userID
+                        WHERE c.userID IN (
+                            SELECT blockedUserID
+                            FROM blocklist
+                            WHERE userID = %s
+                            UNION
+                            SELECT userID
+                            FROM blocklist
+                            WHERE blockedUserID = %s
+                        )
                     ) p
                     WHERE p.contentID = c.contentID
                 )
                 ORDER BY RANDOM()
                 LIMIT 3;
-            """, (uid,uid))
+            """, (uid,uid,uid,uid))
             rows = cur.fetchall()
         return rows
     except psycopg2.Error as e:
@@ -40,19 +53,64 @@ def get_recent_history_ids(uid):
         if conn:
             release_connection(conn)
 
+def get_history_ran(uid):
+    conn = None
+    try:
+        conn = get_connection()
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT c.contentid, c.title
+                FROM content c
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM (
+                        SELECT lastcontetid AS contentID
+                        FROM "user"
+                        WHERE userID = %s AND lastcontetid IS NOT NULL
+                        UNION
+                        SELECT DISTINCT contentID
+                        FROM content
+                        WHERE userID IN (
+                            SELECT blockedUserID FROM blocklist WHERE userID = %s
+                            UNION
+                            SELECT userID FROM blocklist WHERE blockedUserID = %s
+                        )
+                    ) p
+                    WHERE p.contentID = c.contentID
+                )
+                ORDER BY RANDOM()
+                LIMIT 3;
+            """, (uid,uid,uid))
+            rows = cur.fetchall()
+        return rows
+    except psycopg2.Error as e:
+        print("データベースエラー:", e)
+        return []
+    finally:
+        if conn:
+            release_connection(conn)
 
-# SELECT DISTINCT contentID
-# FROM (
-#     SELECT contentID
-#     FROM playhistory
-#     WHERE userID = '24m1kumuhUaYZOKyH3YraLLzFnX2'
-#     ORDER BY playid DESC
-#     LIMIT 50
-# ) AS recent_history
-# UNION
-# SELECT lastcontetid
-# FROM "user"
-# WHERE userID = '24m1kumuhUaYZOKyH3YraLLzFnX2';
+# SELECT c.contentid, c.title
+# FROM content c
+# WHERE NOT EXISTS (
+#     SELECT 1
+#     FROM (
+#         SELECT lastcontetid AS contentID
+#         FROM "user"
+#         WHERE userID = '24m1kumuhUaYZOKyH3YraLLzFnX2' AND lastcontetid IS NOT NULL
+#         UNION
+#         SELECT DISTINCT contentID
+#         FROM content
+#         WHERE userID IN (
+#             SELECT blockedUserID FROM blocklist WHERE userID = '24m1kumuhUaYZOKyH3YraLLzFnX2'
+#             UNION
+#             SELECT userID FROM blocklist WHERE blockedUserID = '24m1kumuhUaYZOKyH3YraLLzFnX2'
+#         )
+#     ) p
+#     WHERE p.contentID = c.contentID
+# )
+# ORDER BY RANDOM()
+# LIMIT 3;
 
 
 # SELECT c.contentid, c.title
@@ -72,6 +130,19 @@ def get_recent_history_ids(uid):
 #         SELECT lastcontetid
 #         FROM "user"
 #         WHERE userID = '24m1kumuhUaYZOKyH3YraLLzFnX2'
+#         UNION
+#         SELECT c.contentID
+#         FROM content c
+#         JOIN "user" u ON c.userID = u.userID
+#         WHERE c.userID IN (
+#             SELECT blockedUserID
+#             FROM blocklist
+#             WHERE userID = '24m1kumuhUaYZOKyH3YraLLzFnX2'
+#             UNION
+#             SELECT userID
+#             FROM blocklist
+#             WHERE blockedUserID = '24m1kumuhUaYZOKyH3YraLLzFnX2'
+#         )
 #     ) p
 #     WHERE p.contentID = c.contentID
 # )
